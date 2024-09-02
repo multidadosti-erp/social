e3e8e96d69881ad483457ff3418cc7b470f1456e
Augusto Costa <gustotc@gmail.com>
2024-09-02 16:56:52 -0300

    Adiciona método 'search' campo 'state' módulo 'mail_activity_done'

--- mail_activity_done/models/mail_activity.py
+++ mail_activity_done/models/mail_activity.py
@@ -1,64 +1,96 @@
 # Copyright 2018 Eficent <http://www.eficent.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from odoo import api, fields, models, _
+from datetime import date
 
 
 class MailActivity(models.Model):
 
-    _inherit = 'mail.activity'
+    _inherit = "mail.activity"
 
     active = fields.Boolean(
-        default=True
+        default=True,
     )
 
     done = fields.Boolean(
-        default=False
+        default=False,
     )
 
     state = fields.Selection(
-        selection_add=[
-            ('done', 'Done')
-        ],
+        selection_add=[("done", "Done")],
         store=False,
-        compute='_compute_state'
+        compute="_compute_state",
+        search="_search_state",
     )
 
     date_done = fields.Date(
-        'Completed Date',
+        "Completed Date",
         index=True,
         readonly=True,
     )
 
     status = fields.Selection(
         selection=[
-            ('active', 'Active'),
-            ('canceled', 'Canceled'),
-            ('completed', 'Completed'),
+            ("active", "Active"),
+            ("canceled", "Canceled"),
+            ("completed", "Completed"),
         ],
-        default='active'
+        default="active",
     )
 
     type_id_show_on_plan_activities = fields.Boolean(
-        related='activity_type_id.show_on_plan_activities'
+        related="activity_type_id.show_on_plan_activities",
     )
 
-    @api.depends('date_deadline', 'done')
+    def _search_state(self, operator, operand):
+        """Método para Pesquisar o Campo State que é computed
+
+        Returns:
+            domain: Lista dos Ids a Mostrar
+        """
+        today_default = date.today()
+
+        if operand == "done":
+            domain = [("done", operator, True)]
+        elif operand == "overdue":
+            if operator == "=":
+                domain = [("date_deadline", "<", today_default)]
+            else:
+                domain = [("date_deadline", ">=", today_default)]
+        elif operand == "today":
+            if operator == "=":
+                domain = [("date_deadline", "=", today_default)]
+            else:
+                domain = [("date_deadline", "!=", today_default)]
+        elif operand == "planned":
+            if operator == "=":
+                domain = [("date_deadline", ">", today_default)]
+            else:
+                domain = [("date_deadline", "<=", today_default)]
+        else:
+            domain = [("1", "=", "0")]
+
+        ids = self.search(domain).ids
+
+        return [("id", "in", ids)]
+
+    @api.depends("date_deadline", "done")
     def _compute_state(self):
         super(MailActivity, self)._compute_state()
         for record in self.filtered(lambda activity: activity.done):
-            record.state = 'done'
+            record.state = "done"
 
 
 class MailActivityMixin(models.AbstractModel):
 
-    _inherit = 'mail.activity.mixin'
+    _inherit = "mail.activity.mixin"
 
     activity_ids = fields.One2many(
         domain=lambda self: [
-            ('res_model', '=', self._name),
-            ('active', '=', True),
-            ('done', '=', False),
-            ('type_id_show_on_plan_activities', '=', True)
+            ("res_model", "=", self._name),
+            ("active", "=", True),
+            ("done", "=", False),
+            ("type_id_show_on_plan_activities", "=", True),
         ]
     )
 
@@ -70,11 +102,11 @@ class MailActivityMixin(models.AbstractModel):
             [list]: Ids das Atividades por State
         """
         return self.activity_ids.filtered(
-                lambda x: not x.done and x.status == 'active'
-            ).mapped('state')
+            lambda x: not x.done and x.status == "active"
+        ).mapped("state")
 
     def _get_activity_where_statement(self):
-        """ Adiciona condição em cláusula where de sql utilizado em
+        """Adiciona condição em cláusula where de sql utilizado em
         função do core para obter atividades atrasadas. Adiciona as
         condições para pegar apenas atividades que não foram concluídas
         (done = False) e atividades que estão ativas (status = 'active').
@@ -87,7 +119,7 @@ class MailActivityMixin(models.AbstractModel):
         return res
 
     def _search_activity_date_deadline(self, operator, operand):
-        """ Atualiza método search, para considerar apenas atividades que
+        """Atualiza método search, para considerar apenas atividades que
         ainda não foram concluídas.
 
         Args:
@@ -97,6 +129,8 @@ class MailActivityMixin(models.AbstractModel):
         Returns:
             list: domain do search
         """
-        res = super(MailActivityMixin, self)._search_activity_date_deadline(operator, operand)
-        res.append(('activity_ids.done', '=', False))
+        res = super(MailActivityMixin, self)._search_activity_date_deadline(
+            operator, operand
+        )
+        res.append(("activity_ids.done", "=", False))
         return res
