e6dfed23ab58b4cdf74efa3588e4f83665ca8e2d
Fernando Saeta <fernando.r.saeta@gmail.com>
2025-02-18 14:37:35 -0300

    Comentários em código ref a Equipes de Atividades
    - Mantém código ref. a equipes de atividades comentados para
      implementação posterior na atribuição de atividades por equipes.

--- mail_activity_team/__manifest__.py
+++ mail_activity_team/__manifest__.py
@@ -20,7 +20,7 @@
         'security/ir.model.access.csv',
         'security/mail_activity_team_security.xml',
         'views/mail_activity_team_views.xml',
-        'views/mail_activity_views.xml',
+        # 'views/mail_activity_views.xml',
         'views/res_users_views.xml',
     ],
     'demo': [
